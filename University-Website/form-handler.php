f3fc7dbbc2edfd3c6eb867a61590a8bb12247016
MuhammadAsimKundi <muhammadasimkundi@gmail.com>
2026-02-18 12:56:27 +0500

    added contact page. and website completed.

---
+++ University-Website/form-handler.php
@@ -0,0 +1,25 @@
+<!-- commented all the code because it will only work when live  so that's i commented them.-->
+
+<?php
+// $name = $_POST['name'];
+// $visitor_email = $_POST['subject'];
+// $message = $_POST['messsage'];
+
+// $email_form = 'info@yourwebsite.com';
+
+// $email_subject = 'New Form Submission';
+
+// $email_body = "User Name: $name.\n".
+//                 "User Email: $visitor_email.\n".
+//                 "Subject: $subject.\n".
+//                 "User Message: $message .\n";
+
+// $to = 'muhammadasimkundi@gmail.com';
+// $headers = "Form: $email_from \r\n";
+// $headers .= "Reply-To: $visitor_email \r\n";
+
+// mail($to,$email_subject,$email_body,$headers);
+
+// header("Location: contact.html")
+
+?>
